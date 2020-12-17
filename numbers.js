76ac4e8ece24aba74920d5a562c8ba0fea7341ec
SarahRector <rector.sarah.r@gmail.com>
2020-12-17 13:21:51 -0800

    indents!

--- numbers.js
+++ numbers.js
@@ -1,9 +1,9 @@
 export default function compareNumbers(myInput, correctNumber) {
-  if (myInput === correctNumber) {
-    return 0;
-  } else if (myInput < correctNumber) {
-    return -1;
-  } else if (myInput > correctNumber) {
-    return 1;
+    if (myInput === correctNumber) {
+      return 0;
+    } else if (myInput < correctNumber) {
+      return -1;
+    } else if (myInput > correctNumber) {
+      return 1;
+    }
   }
-}
